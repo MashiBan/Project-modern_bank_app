7eb43052f3d6099378e7a275344989dbb4c9b911
frypan <fry@pan.local>
2022-09-26 03:54:03 -0700

    Modern Bank App Project .

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+const Person = (props) =>{
+  return(
+    <>
+    <h1>Name:{props.name}</h1>
+    <h2>Last Name:Doe</h2>
+    <h2>Age: 30</h2>
+    </>
+  )
+}
+const App = () => {
+  const name= 'John';
+  const isNameShowing = false;
+  return (
+    <div className="App">
+  <Person name={'jane'}/>
+  <Person name={'Martin'}/>
+
+  
+    </div>
+  );
+}
+
+export default App;
